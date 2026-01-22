403b21a00bcfeb3595fb9618c52f15c05a2afbde
Vector404 <victornarazas2005@gmail.com>
2026-01-22 12:37:29 -0500

    Modificando entidades en Angular

--- userService/userService/angular/src/app/shared/model/evento.model.ts
+++ userService/userService/angular/src/app/shared/model/evento.model.ts
@@ -1,14 +1,14 @@
-import { Tematica } from "./tematica.model";
+import { Tematica } from './tematica.model';
 
-export interface Evento{
-    idEvento: number;
-    nombre: string;
-    descripcion: string;
-    tematica: Tematica;
-    fecha: string;
-    hora: string;
-    precio: number;
-    aforo: number;
-    //cupos: number;
-    imagen: string;
+export interface Evento {
+  idEvento: number;
+  nombre: string;
+  descripcion: string;
+  tematica: Tematica;
+  fecha: string;
+  hora: string;
+  precio: number;
+  aforo: number;
+  cupos: number;
+  imagen: string;
 }
--- userService/userService/angular/src/app/shared/model/usuario.model.ts
+++ userService/userService/angular/src/app/shared/model/usuario.model.ts
@@ -1,21 +1,21 @@
-import { Distrito } from "./distrito.model";
-import { Rol } from "./rol.model";
+import { TipoDocumento } from '../enums/tipoDocumento.enum';
+import { Distrito } from './distrito.model';
+import { Rol } from './rol.model';
 
-export interface Usuario{
-    idUsuario: number;
-    nombre: string;
-    apePaterno: string;
-    apeMaterno: string;
-    correo: string;
-    clave: string;
-    tipoDocumento: string; // DNI, Carnet de Extranjeria, Pasaporte, Cartilla Militar FALTA AGREGAR ENUM
-    nroDoc: string;
-    imagen?: string;
-    direccion: string;
-    telefono: string;
-    distrito: Distrito;
-    rol: Rol;
-    fechaRegistro: string;
-    estado: boolean;
-
+export interface Usuario {
+  idUsuario: number;
+  nombre: string;
+  apePaterno: string;
+  apeMaterno: string;
+  correo: string;
+  clave: string;
+  tipoDocumento: TipoDocumento;
+  nroDoc: string;
+  imagen?: string;
+  direccion: string;
+  telefono: string;
+  distrito: Distrito;
+  rol: Rol;
+  fechaRegistro: string;
+  estado: boolean;
 }
