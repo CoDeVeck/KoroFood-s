ece74b41ac4735a4abbc9d67002cc61315abdb90
Vector404 <victornarazas2005@gmail.com>
2026-02-13 12:37:04 -0500

    🔧  chore(order): agregando config websocket

--- orderService/orderService/src/main/java/com/koroFoods/orderService/Config/WebSocketListener.java
+++ orderService/orderService/src/main/java/com/koroFoods/orderService/Config/WebSocketListener.java
@@ -1,4 +1,26 @@
 package com.koroFoods.orderService.Config;
 
-public class WebSocketConfig {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketListener {
+
+
+    private final SimpMessageSendingOperations messageTemplate;
+
+    //Capturamos el momento en que un cliente se va
+    @EventListener
+    public void handleWebSocketDisconnectListener(
+            SessionDisconnectEvent event
+    ) {
+        log.info("User disconnected");
+
+    }
 }
